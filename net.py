cad9a0f9dfa2710a3ad775afba9582c7d95bf554
Naoto Inoue <kingfisher0804@gmail.com>
2018-04-16 14:07:33 +0900

    Update net.py

--- net.py
+++ net.py
@@ -12,9 +12,9 @@ from torch.nn import init
 def weights_init_normal(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        init.normal(m.weight.data, 0.0, 0.2)
+        init.normal(m.weight.data, 0.0, 0.02)
     elif classname.find('Linear') != -1:
-        init.normal(m.weight.data, 0.0, 0.2)
+        init.normal(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm2d') != -1:
         init.normal(m.weight.data, 1.0, 0.02)
         init.constant(m.bias.data, 0.0)
