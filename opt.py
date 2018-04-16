0bd9d581c1aa066db43352c370c4b2aeee629f85
Naoto Inoue <kingfisher0804@gmail.com>
2018-04-16 21:46:21 +0900

    Update opt.py

--- opt.py
+++ opt.py
@@ -1,16 +1,3 @@
-# params = {
-#     'res_image': 32,
-#     'n_class': 10,
-#     'base_lr': 1e-3,
-#     'weight_decay': 1e-5,
-#     'betas': (0.5, 0.999),
-#     'loss': {'dis': 1.0, 'gen': 1.0, 'task': 1.0},
-#     'gen': {'n_ch': 32, 'n_hidden': 10, 'n_resblock': 6},
-#     'dis': {'n_ch': 128, 'dr_prob': 0.1, 'noise_sigma': 0.2}
-# }
-
-
-# SBADA-GAN
 params = {
     'batch_size': 32, 'base_lr': 1e-4, 'num_epochs': 500,
     'weight_decay': 1e-5, 'pool_size': 50,
