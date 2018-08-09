452d2462e285940f9f10618d530d4c09ea27a19c
Naoto Inoue <inoue@hal.t.u-tokyo.ac.jp>
2018-08-09 21:32:37 +0900

    update README

---
+++ methods/sbada-gan/README.md
@@ -0,0 +1,40 @@
+# pytorch-SBADA-GAN
+
+This is an unofficial pytorch implementation of a paper, From source to target and back: symmetric bi-directional adaptive GAN [Russo+, CVPR2018].
+
+Please note that this is an ongoing project and I cannot fully reproduce the results currently.
+
+
+## Usage
+
+These examples are for the MNIST to USPS experiment.
+
+### Train `Source Only` Model
+```
+CUDA_VISIBLE_DEVICES=<gpu_id> python train_unsup.py --exp mnist_usps
+```
+
+### Train `Target Only` Model
+```
+CUDA_VISIBLE_DEVICES=<gpu_id> python train_sup.py --exp mnist_usps
+```
+
+### Train SBADA-GAN Model
+```
+CUDA_VISIBLE_DEVICES=<gpu_id> python train_sbada_gan.py --exp mnist_usps
+```
+
+## Results
+Accuracy [%] is shown. Note that the values are slightly different from the original paper [1].
+Each model is trained for 200 epochs once.
+
+| | MNIST->USPS | USPS->MNIST |
+:---:|:----:|:----:
+| Source Only | 78.7 | 60.7 |
+| SBADA-GAN C_{t} | 95.9 | 92.9 |
+| SBADA-GAN C_{s} | 96.1 | 96.2 |
+| SBADA-GAN | 96.7 | 96.5 |
+| Target Only | 96.1 | 99.3 |
+
+## References
+- [1]: R. Paolo et al. "From source to target and back: symmetric bi-directional adaptive GAN.", in CVPR, 2018.
---
+++ test_classifier.py
@@ -0,0 +1,51 @@
+import click
+import torch
+import torch.cuda
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+
+from util.datasets import DADataset, load_source_target_datasets
+from util.io import get_config, load_model
+from util.net import LenetClassifier
+from util.opt import exp_list
+from util.preprocess import get_composed_transforms
+
+torch.backends.cudnn.benchmark = True
+
+
+@click.command()
+@click.option('--exp', type=click.Choice(exp_list), required=True)
+@click.option('--snapshot', type=str, required=True)
+def experiment(exp, snapshot):
+    config = get_config('config.yaml')
+    batch_size = int(config['batch_size'])
+
+    device = torch.device('cuda')
+    _, tgt = load_source_target_datasets(exp)
+    n_ch_t = tgt.train_X.shape[1]  # number of color channels
+    res = tgt.train_X.shape[-1]  # size of image
+    n_classes = tgt.n_classes
+
+    cls = LenetClassifier(n_classes, n_ch_t, res).to(device)
+    load_model(cls, snapshot)
+
+    test_tfs = get_composed_transforms(train=False, hflip=False)
+    test = DADataset(tgt.test_X, tgt.test_y, test_tfs, False)
+    test_loader = DataLoader(
+        test, batch_size=batch_size * 4, num_workers=4)
+
+    print('Testing...')
+    cls.eval()
+
+    n_err = 0
+    with torch.no_grad():
+        for tgt_x, tgt_y in test_loader:
+            prob_y = F.softmax(cls(tgt_x.to(device)), dim=1)
+            pred_y = torch.max(prob_y, dim=1)[1]
+            pred_y = pred_y.to(torch.device('cpu'))
+            n_err += (pred_y != tgt_y).sum().item()
+    print('Err:', n_err / len(test))
+
+
+if __name__ == '__main__':
+    experiment()
